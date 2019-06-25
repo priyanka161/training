8ed806d7a0ab8eb01dda3edaa1ba1431dcca505c
coolarun <coolarun4591@gmail.com>
2019-06-25 16:56:34 +0530

    mouseover

---
+++ script.js
@@ -0,0 +1,11 @@
+var x = 0;
+var y = 0;
+var z = 0;
+
+function myMoveFunction() {
+  document.getElementById("demo").innerHTML = z+=1;
+}
+
+function myOverFunction() {
+  document.getElementById("demo1").innerHTML = y+=1;
+}
